0c30f76ea8871d78545712b800e83e292691e3ae
jisokoo <abarbancon29@gmail.com>
2022-09-07 22:35:56 +0200

    basic structure and style in place

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import './App.css';
+import './index.css';
+import Dice from './Components/Dice';
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <h1>Tenzies!</h1>
+        <p>Simple: try to end with all dice showing the same number by holding any you want and relaunching. Try to do this with the least attempts possible. Good luck!</p>
+        <Dice />
+        <button>Click Here</button>
+      </main>
     </div>
   );
 }
---
+++ src/Components/Dice.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Die from "./Die";
+
+export default function Dice() {
+    return (
+        <div className="dice--section">
+            <Die />
+            <Die />
+            <Die />
+            <Die />
+            <Die />
+            <Die />
+            <Die />
+            <Die />
+            <Die />
+            <Die />
+        </div>
+        
+    )
+}
