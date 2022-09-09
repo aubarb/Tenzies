e713d018a2351d1861854120de84a1cf03a96e46
jisokoo <abarbancon29@gmail.com>
2022-09-09 13:52:36 +0200

    Add real dice instead of numbers

--- src/Components/Die.js
+++ src/Components/Die.js
@@ -4,11 +4,13 @@ export default function Die({ value, isHeld, id,  onClick }) {
 
     return (
         <div 
-            className={!isHeld ? "die" : "die_held"}
             key={id}
             onClick={() => onClick(id, isHeld)}
         >
-            <p>{value}</p>
+            <img 
+                src={require(`../Images/Die${value}.png`)} 
+                className={!isHeld ? "die" : "die_held"}
+            />
         </div>
     )
 }
